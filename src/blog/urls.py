ca4011b8a4ebf6d1f72d2da3845c84f4dcc49ee0
iprogrammer1390 <iprogrammer1390@gmail.com>
2022-10-03 00:40:09 +0600

    Созданы все crud вью

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -4,9 +4,22 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from posts.views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('', PostListView.as_view(), name='list'),
+    path('create/', PostCreateView.as_view(), name='create'),
+    path('<slug>/', PostDetailView.as_view(), name='detail'),
+    path('<slug>/update/', PostUpdateView.as_view(), name='update'),
+    path('<slug>/delete/', PostDeleteView.as_view(), name='delete'),
 ]
 
 if settings.DEBUG:
--- src/posts/views.py
+++ src/posts/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Post, PostView, Like, Comment
 
-# Create your views here.
+
+class PostListView(ListView):
+    model = Post
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ('title', 'content', 'thumbnail', 'author', 'slug')
+
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = ('title', 'content', 'thumbnail', 'author', 'slug')
+
+
+class PostDeleteView(DeleteView):
+    model = Post
+    success_url = '/'
